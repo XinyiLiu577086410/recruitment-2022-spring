0c1c483a9290f3df8fc2b3102095549fb11edfa2
liuxinyi <577086410@qq.com>
2023-04-05 18:48:27 +0800

    [mandelbrot thread]:            [84.778] ms Wrote image file mandelbrot-thread.ppm (4.70x speedup from 8 threads)

--- task1-multi-thread/main.cpp
+++ task1-multi-thread/main.cpp
@@ -65,13 +65,17 @@ bool verifyResult (int *gold, int *result, int width, int height) {
 
     return 1;
 }
-
+int k;
 int main(int argc, char** argv) {
-
+    if( argv[2][0] == '1' ) k = 0;
+    else k = 1;
+    // for(int i=0;i<argc;i++){
+    //     printf("%s\n", argv[i]);
+    // }
     const unsigned int width = 1600;
     const unsigned int height = 1200;
     const int maxIterations = 256;
-    int numThreads = 2;
+    int numThreads = 8;
 
     float x0 = -2;
     float x1 = 1;
@@ -145,7 +149,7 @@ int main(int argc, char** argv) {
 
     double minThread = 1e30;
     for (int i = 0; i < 5; ++i) {
-      memset(output_thread, 0, width * height * sizeof(int));
+        memset(output_thread, 0, width * height * sizeof(int));
         double startTime = CycleTimer::currentSeconds();
         mandelbrotThread(numThreads, x0, y0, x1, y1, width, height, maxIterations, output_thread);
         double endTime = CycleTimer::currentSeconds();
